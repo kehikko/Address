0e4953fe94de056f93642d074b5286e7948a5948
Antti Partanen <aehparta@iki.fi>
2018-01-14 19:54:34 +0200

    several updates, postcode import from csv etc

---
+++ Postcodes.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Address;
+
+class Postcodes
+{
+	public static function fromCsv($country, $file, array $columns, $delimiter = ',', $enclosure = '"', $escape = '\\')
+	{
+		/* try to open csv */
+		$f = fopen($file, 'r');
+		if (!$f)
+		{
+			throw new \Exception('unable to open postcodes source csv file for reading: ' . $file);
+		}
+		/* read contents */
+		$postcodes = array();
+		$headers   = null;
+		while (($data = fgetcsv($f, 0, $delimiter, $enclosure, $escape)) !== false)
+		{
+			$n = count($data);
+			if ($n < count($columns))
+			{
+				continue;
+			}
+			if ($headers)
+			{
+				$fields   = array('postcode', 'locality', 'city', 'state');
+				$postcode = array();
+				foreach ($fields as $field)
+				{
+					$column = isset($columns[$field]) ? $columns[$field] : null;
+					$value  = null;
+					if ($column)
+					{
+						$value = $data[$headers[$column]];
+					}
+					$postcode[$field] = $value;
+				}
+				$postcodes[] = $postcode;
+			}
+			else
+			{
+				$headers = array();
+				for ($i = 0; $i < $n; $i++)
+				{
+					$headers[$data[$i]] = $i;
+				}
+				foreach ($columns as $field => $column)
+				{
+					if (!isset($headers[$column]))
+					{
+						throw new \Exception('missing header: ' . $column);
+					}
+				}
+			}
+		}
+
+		/* write to database */
+		$country_code = $country->getAlpha2();
+		$total        = 0;
+		$new          = 0;
+		$em           = \kernel::getInstance()->getEntityManager();
+		foreach ($postcodes as $postcode)
+		{
+			$p = Postcode::find($country_code, $postcode['postcode']);
+			if (!$p)
+			{
+				$p = new Postcode($country_code, $postcode['postcode']);
+				$new++;
+			}
+			$p->setLocality($postcode['locality']);
+			$p->setCity($postcode['city']);
+			$p->setState($postcode['state']);
+			$em->persist($p);
+			$total++;
+		}
+		$em->flush();
+		\kernel::log(LOG_INFO, 'postcodes populated for country: ' . $country->getName() . ', total found: ' . $total . ', added new: ' . $new);
+	}
+}
